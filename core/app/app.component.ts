202c8ba3a487824b15153dd7473d3f6931ce8e2e
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-17 20:59:10 +0900

    パラメータをインポートできるように（ボタン・ドラッグ＆ドロップ）

--- core/app/app.component.ts
+++ core/app/app.component.ts
@@ -8,11 +8,13 @@ import {ShapePropertyModal} from "./shape-property.component";
 import {StageComponent} from "./stage.component";
 import {ViewChild} from "angular2/core";
 import {ParticleShapeTypes} from "./particle-canvas/particle-shape-types";
+import {ElementRef} from "angular2/core";
 
 const template = `
-  <div class="col-sm-7 col-xs-12">
-      <stage #stageComponent [drawingData]="drawingData"></stage>
-  </div>
+<div class="container">
+    <div class="col-sm-7 col-xs-12">
+        <stage #stageComponent [drawingData]="drawingData"></stage>
+    </div>
   <nav id="propertyPanel" class="navbar navbar-fixed-top">
       <property-panel [drawingData]="drawingData"
       (exportSVGEvent)="handleSVGClick()"
@@ -20,31 +22,38 @@ const template = `
       (exportPNGEvent)="handlePNGClick()"
       ></property-panel>
   </nav>
-  <shape-property-modal [drawingData]="drawingData"></shape-property-modal>
+    <shape-property-modal [drawingData]="drawingData"></shape-property-modal>
+</div>
 `;
 
 @Component({
   selector: `my-app`,
   template: template,
-  directives: [StageComponent, PropertyPanel,ShapePropertyModal],
+  directives: [StageComponent, PropertyPanel, ShapePropertyModal],
 })
 
 export class AppComponent {
   private drawingData:DrawingData;
   @ViewChild("stageComponent") stageComponent:StageComponent;
+  @ViewChild("propertyPanel") propertyPanel:PropertyPanel;
 
-  handleSVGClick(){
+  private hogehoge = () => {
+
+  };
+
+  handleSVGClick() {
     this.stageComponent.exportSVG().then(this.openSVGExportWindow);
   }
-  openSVGExportWindow = () =>{
-    window.open("data:image/svg+xml,\n"+encodeURIComponent(this.stageComponent.getParticleSVGString()));
+
+  openSVGExportWindow = () => {
+    window.open("data:image/svg+xml,\n" + encodeURIComponent(this.stageComponent.getParticleSVGString()));
+  };
+
+  handleExportParamaterClick() {
+    window.open("data:text/plain;charset=UTF-8,\n" + encodeURIComponent(JSON.stringify(this.drawingData)));
   }
 
-  handleExportParamaterClick(){
-    console.log("handleExportParamaterClick");
-  }
-
-  constructor() {
+  constructor(element:ElementRef) {
     this.drawingData = new DrawingData();
   }
 }
