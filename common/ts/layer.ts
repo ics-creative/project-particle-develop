224252a05c38c621855a4df6e4be3af2cc4e631b
tonkotsuboy <t.kano.624@gmail.com>
2016-01-18 10:17:26 +0900

    ソース整頓：不要になったcommonフォルダを削除

--- common/ts/layer.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nyamogera on 2016/01/06.
- */
-
-interface ILayer{
-  isExit(): boolean ;
-  update() : void;
-}
--- common/ts/svg-exporter.d.ts
+++
@@ -1,5 +0,0 @@
-declare class SVGExporter {
-  constructor(stage:createjs.Container,value1:boolean,value2:boolean,value3:boolean);
-  svg:Node;
-  run():void;
-}
