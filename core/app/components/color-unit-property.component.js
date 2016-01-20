398304650e090c421f057814a348a404b826bfff
ics-ikeda <ikeda@ics-web.jp>
2016-01-21 00:02:23 +0900

    コンポーネントのHTML編集 : input-range タグの適用 : カラーの設定

--- core/app/components/color-unit-property.component.js
+++ core/app/components/color-unit-property.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core"], function(exports_1) {
+System.register(["angular2/core", "./input-range.component"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,12 +8,15 @@ System.register(["angular2/core"], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, input_range_component_1;
     var ColorUnitPropertyPanel;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (input_range_component_1_1) {
+                input_range_component_1 = input_range_component_1_1;
             }],
         execute: function() {
             ColorUnitPropertyPanel = (function () {
@@ -23,7 +26,8 @@ System.register(["angular2/core"], function(exports_1) {
                     core_1.Component({
                         selector: "color-unit-property-panel",
                         templateUrl: "app/components/template/color-unit-property.html",
-                        inputs: ["colorData"]
+                        inputs: ["colorData"],
+                        directives: [input_range_component_1.InputRangeComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], ColorUnitPropertyPanel);
--- core/app/components/color-unit-property.component.ts
+++ core/app/components/color-unit-property.component.ts
@@ -1,10 +1,12 @@
 import {Component} from "angular2/core";
 import {ColorData} from "../data/color-data";
+import {InputRangeComponent} from "./input-range.component";
 
 @Component({
   selector: "color-unit-property-panel",
   templateUrl: "app/components/template/color-unit-property.html",
-  inputs: ["colorData"]
+  inputs: ["colorData"],
+  directives: [InputRangeComponent]
 })
 export class ColorUnitPropertyPanel {
   private colorData:ColorData;
