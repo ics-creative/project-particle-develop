c58e258b71deafc778968c2ca26b027998d9e74a
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-20 19:26:03 +0900

    パーティクル：テンプレの追加（ハート）

--- core/app/assets/particle-paramater.js
+++ core/app/assets/particle-paramater.js
@@ -100,9 +100,56 @@ System.register([], function(exports_1) {
                             "luminanceVariance": 0
                         }
                     };
+                    this.setting3 = {
+                        "bgColor": "#00000",
+                        "width": 500,
+                        "height": "500",
+                        "friction": "0.021",
+                        "startX": "242",
+                        "startXVariance": "0",
+                        "startY": "396",
+                        "startYVariance": "0",
+                        "lifeSpan": 100,
+                        "lifeSpanVariance": 30,
+                        "initialDirection": 270,
+                        "initialDirectionVariance": "136.8",
+                        "initialSpeed": "6",
+                        "initialSpeedVariance": 0.5,
+                        "accelerationSpeed": 0,
+                        "accelerationDirection": 0,
+                        "startAlpha": 1,
+                        "startAlphaVariance": 0,
+                        "finishAlpha": "0",
+                        "finishAlphaVariance": "0",
+                        "startScale": "1.81",
+                        "startScaleVariance": 0,
+                        "finishScale": 1,
+                        "finishScaleVariance": 0,
+                        "shapeIdList": [
+                            "heart"
+                        ],
+                        "emitFrequency": 1,
+                        "startColor": {
+                            "hue": "355.74",
+                            "hueVariance": 0,
+                            "satuation": 75,
+                            "satuationVariance": 0,
+                            "luminance": 75,
+                            "luminanceVariance": 0
+                        },
+                        "finishColor": {
+                            "hue": "352.63",
+                            "hueVariance": 0,
+                            "satuation": 75,
+                            "satuationVariance": 0,
+                            "luminance": 75,
+                            "luminanceVariance": 0
+                        }
+                    };
                     this.list = new Array();
                     this.list.push(this.setting1);
                     this.list.push(this.setting2);
+                    this.list.push(this.setting3);
                 }
                 return ParticleParamater;
             })();
