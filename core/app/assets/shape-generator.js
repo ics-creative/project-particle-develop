f1fd4cad9cfde90fe14a30011631f6fdf2cc5cf6
ics-ikeda <ikeda@ics-web.jp>
2016-01-21 02:50:16 +0900

    シェイプジェネレーターのコードをシンプルに変更 (わざわざ Object のパラメーターをコピーしなくてもよい)

--- core/app/assets/shape-generator.js
+++ core/app/assets/shape-generator.js
@@ -9,18 +9,7 @@ System.register([], function(exports_1) {
         execute: function() {
             ShapeGenerator = (function () {
                 function ShapeGenerator() {
-                    this.shapeObjects = new Object();
-                    this.shapeObjects["blur_circle"] = lib.blur_circle;
-                    this.shapeObjects["circle"] = lib.circle;
-                    this.shapeObjects["flower"] = lib.flower;
-                    this.shapeObjects["heart"] = lib.heart;
-                    this.shapeObjects["kirakira"] = lib.kirakira;
-                    this.shapeObjects["kirakira2"] = lib.kirakira2;
-                    this.shapeObjects["reverse_blur_circle"] = lib.reverse_blur_circle;
-                    this.shapeObjects["square"] = lib.square;
-                    this.shapeObjects["star"] = lib.star;
-                    this.shapeObjects["star_10"] = lib.star_10;
-                    this.shapeObjects["triangle"] = lib.triangle;
+                    this.shapeObjects = lib;
                 }
                 ShapeGenerator.prototype.generateShape = function (id) {
                     return new this.shapeObjects[id]();
--- core/app/assets/shape-generator.ts
+++ core/app/assets/shape-generator.ts
@@ -7,19 +7,7 @@ export class ShapeGenerator {
   shapeObjects:Object;
 
   constructor () {
-    this.shapeObjects = new Object();
-    this.shapeObjects["blur_circle"] = lib.blur_circle
-    this.shapeObjects["circle"] = lib.circle
-    this.shapeObjects["flower"] = lib.flower
-    this.shapeObjects["heart"] = lib.heart
-    this.shapeObjects["kirakira"] = lib.kirakira
-    this.shapeObjects["kirakira2"] = lib.kirakira2
-    this.shapeObjects["reverse_blur_circle"] = lib.reverse_blur_circle
-    this.shapeObjects["square"] = lib.square
-    this.shapeObjects["star"] = lib.star
-    this.shapeObjects["star_10"] = lib.star_10
-    this.shapeObjects["triangle"] = lib.triangle
-
+    this.shapeObjects = lib;
   }
 
   generateShape(id:string) {
