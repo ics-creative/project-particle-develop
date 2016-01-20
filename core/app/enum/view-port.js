0ad0667a53e10ec94e6c09429be7dc4ac64dd560
ics-kano <takeshi.kano@ics-web.jp>
2016-01-20 14:46:29 +0900

    UI設計：モバイル版でのボタン表示

---
+++ core/app/enum/view-port.js
@@ -0,0 +1,13 @@
+System.register([], function(exports_1) {
+    var Viewport;
+    return {
+        setters:[],
+        execute: function() {
+            (function (Viewport) {
+                Viewport[Viewport["xs"] = 767] = "xs";
+            })(Viewport || (Viewport = {}));
+            exports_1("Viewport", Viewport);
+        }
+    }
+});
+//# sourceMappingURL=view-port.js.map
---
+++ core/app/enum/view-port.ts
@@ -0,0 +1,3 @@
+export enum Viewport{
+  xs = 767
+}
