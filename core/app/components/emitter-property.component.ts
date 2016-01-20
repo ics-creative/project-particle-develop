b88e024be818e1fe411991484565aa06d7d77cdc
ics-ikeda <ikeda@ics-web.jp>
2016-01-21 00:11:07 +0900

    コンポーネントのHTML編集 : input-range タグの適用 : エミッターの設定

--- core/app/components/emitter-property.component.ts
+++ core/app/components/emitter-property.component.ts
@@ -5,15 +5,18 @@ import {Component} from "angular2/core";
 import {DrawingData} from "../data/drawing-data";
 import {EventEmitter} from 'angular2/core';
 import {ShapeData} from "../data/shape-data";
+import {InputRangeComponent} from "./input-range.component";
 
 @Component({
   selector: "emitter-property-panel",
   templateUrl: "app/components/template/emitter-property.html",
-  inputs: ["drawingData","shapeIdList"]
+  inputs: ["drawingData", "shapeIdList"],
+  directives: [InputRangeComponent]
 })
 
 export class EmitterPropertyPanel {
   private drawingData:DrawingData;
+
   constructor() {
   }
 }
