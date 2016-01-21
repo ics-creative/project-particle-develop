39ff8f2466d05fde15a2e005db33c50d7bb426a7
ics-ikeda <ikeda@ics-web.jp>
2016-01-21 19:55:20 +0900

    プロパティーパネルの横幅を変更

--- core/app/enum/canvas-margin.js
+++ core/app/enum/canvas-margin.js
@@ -5,7 +5,7 @@ System.register([], function(exports_1) {
         execute: function() {
             (function (CanvasMargin) {
                 CanvasMargin[CanvasMargin["TOP_DESKTOP"] = 100] = "TOP_DESKTOP";
-                CanvasMargin[CanvasMargin["RIGHT_DESKTOP"] = 294] = "RIGHT_DESKTOP";
+                CanvasMargin[CanvasMargin["RIGHT_DESKTOP"] = 350] = "RIGHT_DESKTOP";
                 CanvasMargin[CanvasMargin["TOP_MOBILE"] = 116] = "TOP_MOBILE";
                 CanvasMargin[CanvasMargin["RIGHT_MOBILE"] = 34] = "RIGHT_MOBILE";
             })(CanvasMargin || (CanvasMargin = {}));
