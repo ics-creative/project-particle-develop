9408bde9622cbaa17366f02946b6fcf2e67d34cd
ics-ikeda <ikeda@ics-web.jp>
2016-01-25 20:50:16 +0900

    ソーシャルボタンの設置

--- core/app/components/app.component.js
+++ core/app/components/app.component.js
@@ -63,8 +63,37 @@ System.register(["angular2/core", "../data/data-drawing", "./property.component"
                     this.drawingData.startY = Math.round(sh / 2);
                     this.drawingData.width = sw;
                     this.drawingData.height = sh;
-                    this.adjustUi();
                 }
+                AppComponent.prototype.ngAfterViewInit = function () {
+                    this.adjustUi();
+                    (function (d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id))
+                            return;
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = "https://b.st-hatena.com/js/bookmark_button.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'hatena'));
+                    !function (d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                        if (!d.getElementById(id)) {
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src = p + '://platform.twitter.com/widgets.js';
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }
+                    }(document, 'script', 'twitter-wjs');
+                    (function (d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id))
+                            return;
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=566926136738876";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                };
                 /**
                  * UIの表示制御
                  */
--- core/app/components/app.component.ts
+++ core/app/components/app.component.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 import {Component} from "angular2/core";
+import {AfterViewInit} from "angular2/core";
 import {DrawingData} from "../data/data-drawing";
 import {PropertyPanel} from "./property.component";
 import {DesktopIOBox} from "./desktop-io.component";
@@ -26,7 +27,7 @@ import {CanvasMargin} from "../enum/canvas-margin";
   ]
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   protected drawingData:DrawingData;
   @ViewChild("stageComponent") stageComponent:StageComponent;
   @ViewChild("propertyPanel") propertyPanel:PropertyPanel;
@@ -57,8 +58,39 @@ export class AppComponent {
     this.drawingData.startY = Math.round(sh / 2);
     this.drawingData.width = sw;
     this.drawingData.height = sh;
+  }
+
+  ngAfterViewInit() {
 
     this.adjustUi();
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://b.st-hatena.com/js/bookmark_button.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'hatena'));
+
+    !function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'twitter-wjs');
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=566926136738876";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   /**
