b8eaa0a5f78eb6191d3372a9b0f1e29235ec72e9
ics-ikeda <ikeda@ics-web.jp>
2016-01-25 19:32:22 +0900

    フッターのデザイン調整

--- core/app/enum/canvas-margin.js
+++ core/app/enum/canvas-margin.js
@@ -5,7 +5,7 @@ System.register([], function(exports_1) {
         setters:[],
         execute: function() {
             (function (CanvasMargin) {
-                CanvasMargin[CanvasMargin["TOP_DESKTOP"] = 90] = "TOP_DESKTOP";
+                CanvasMargin[CanvasMargin["TOP_DESKTOP"] = 135] = "TOP_DESKTOP";
                 CanvasMargin[CanvasMargin["RIGHT_DESKTOP"] = 350] = "RIGHT_DESKTOP";
                 CanvasMargin[CanvasMargin["TOP_MOBILE"] = 116] = "TOP_MOBILE";
                 CanvasMargin[CanvasMargin["RIGHT_MOBILE"] = 3] = "RIGHT_MOBILE";
--- core/app/enum/canvas-margin.ts
+++ core/app/enum/canvas-margin.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 export enum CanvasMargin {
-  TOP_DESKTOP = 90,
+  TOP_DESKTOP = 90 + 45,
   RIGHT_DESKTOP = 350,
   TOP_MOBILE = 116,//116,
   RIGHT_MOBILE = 3
