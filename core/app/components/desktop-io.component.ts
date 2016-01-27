0cc9621c51e12910e8bf0b1653a9f1330b8af662
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-27 21:54:58 +0900

    デスクトップ版の移動作業をしたら、うっかり動かなくなってしまったので戻す。

---
+++ core/app/components/desktop-io.component.ts
@@ -0,0 +1,81 @@
+import {Component, EventEmitter} from "angular2/core";
+import {DrawingData} from "../data/data-drawing";
+import {LocaleData} from "../i18n/locale-data";
+import {LocaleEnData} from "../i18n/locale-en";
+import {LocaleJaData} from "../i18n/locale-ja";
+import {LocaleManager} from "../i18n/locale-manager";
+
+"use strict";
+
+@Component({
+  selector: "desktop-io-box",
+  templateUrl: "app/components-html/desktop-io-box.html",
+  inputs: ["drawingData"],
+  events: [
+    "exportSvgEvent",
+    "exportPngEvent",
+    "exportJpgEvent",
+    "exportWebpEvent",
+    "exportParamaterEvent"
+  ]
+})
+
+export class DesktopIoBox {
+  private exportSvgEvent = new EventEmitter();
+  private exportPngEvent = new EventEmitter();
+  private exportJpgEvent = new EventEmitter();
+  private exportWebpEvent = new EventEmitter();
+  private exportParamaterEvent = new EventEmitter();
+
+  private drawingData:DrawingData;
+
+  constructor(private localeData:LocaleData) {
+  }
+
+  private exportParamater():void {
+    this.exportParamaterEvent.emit(null);
+  }
+
+  private exportSvg():void {
+    this.exportSvgEvent.emit(null);
+  }
+
+  private exportPng():void {
+    this.exportPngEvent.emit(null);
+  }
+
+  private exportJpg():void {
+    this.exportJpgEvent.emit(null);
+  }
+  private exportWebp():void {
+    this.exportWebpEvent.emit(null);
+  }
+
+  private selectParameterFile(obj:any):void {
+    this.importParameterFile(obj.target.files[0])
+  }
+
+  private importParameterFile(file:File):void {
+    // ファイルの内容は FileReader で読み込みます.
+    let fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      // event.target.result に読み込んだファイルの内容が入っています。
+      var json = (<FileReader>event.target).result;
+      let object = JSON.parse(json);
+
+      this.drawingData.into(object);
+    };
+    fileReader.readAsText(file);
+  }
+
+
+
+  private selectEn():void {
+    new LocaleManager().changeLocale(this.localeData, new LocaleEnData());
+  }
+
+  private selectJa():void {
+    new LocaleManager().changeLocale(this.localeData, new LocaleJaData());
+  }
+
+}
