29110eebf7b47de1d0b7525b92bf81be464691c0
ics-nohara <nozomi.nohara@ics-web.jp>
2016-02-02 10:55:44 +0900

    モバイル版：インポートが抜けていたので修正

--- core/app/components/app-mobile.component.js
+++ core/app/components/app-mobile.component.js
@@ -1,18 +1,24 @@
-System.register(["./app.component"], function(exports_1) {
-    "use strict";
+System.register(["../enum/platform-type", "../data/platform-data", "./app.component"], function(exports_1) {
     var __extends = (this && this.__extends) || function (d, b) {
         for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    var app_component_1;
+    var platform_type_1, platform_data_1, app_component_1;
     var MobileAppComponent;
     return {
         setters:[
+            function (platform_type_1_1) {
+                platform_type_1 = platform_type_1_1;
+            },
+            function (platform_data_1_1) {
+                platform_data_1 = platform_data_1_1;
+            },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
             }],
         execute: function() {
+            "use strict";
             MobileAppComponent = (function (_super) {
                 __extends(MobileAppComponent, _super);
                 function MobileAppComponent() {
@@ -22,7 +28,7 @@ System.register(["./app.component"], function(exports_1) {
                     alert("モバイルアプリでは使用不可能です");
                 };
                 MobileAppComponent.prototype.getPlatformData = function () {
-                    return new PlatformData(PlatformType.Mobile);
+                    return new platform_data_1.PlatformData(platform_type_1.PlatformType.Mobile);
                 };
                 MobileAppComponent.prototype.handlePngClick = function () {
                     this.stageComponent.runExportSP().then();
--- core/app/components/app-mobile.component.ts
+++ core/app/components/app-mobile.component.ts
@@ -1,5 +1,9 @@
-"use strict";
+import {PlatformType} from "../enum/platform-type";
+import {PlatformData} from "../data/platform-data";
 import {AppComponent} from "./app.component";
+
+"use strict";
+
 export class MobileAppComponent extends AppComponent {
   protected handleSvgClick() {
     alert("モバイルアプリでは使用不可能です");
