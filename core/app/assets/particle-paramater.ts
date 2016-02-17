ee0d736237a2be3eaf363cbeced5355f6a7d2661
ics-nohara <nozomi.nohara@ics-web.jp>
2016-02-17 13:18:22 +0900

    typo修正（satuation→saturation）

--- core/app/assets/particle-paramater.ts
+++ core/app/assets/particle-paramater.ts
@@ -24,14 +24,14 @@ export class ParticleParamater {
     description : "This is Basic Template",
     imageId : "basic",
     property:
-    {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"60","lifeSpan":"103","lifeSpanVariance":"0","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"7.5","initialSpeedVariance":"0","friction":"0","accelerationSpeed":"0","accelerationDirection":"0","startScale":"0.5","startScaleVariance":"0","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"1","finishAlphaVariance":"0","startX":240.66666666666666,"startXVariance":"0","startY":183.33333333333334,"startYVariance":"0","shapeIdList":["circle"],"startColor":{"hue":"0","hueVariance":"0","satuation":"100","satuationVariance":0,"luminance":"50","luminanceVariance":0},"blendMode":false,"alphaCurveType":"0"}
+    {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"60","lifeSpan":"103","lifeSpanVariance":"0","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"7.5","initialSpeedVariance":"0","friction":"0","accelerationSpeed":"0","accelerationDirection":"0","startScale":"0.5","startScaleVariance":"0","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"1","finishAlphaVariance":"0","startX":240.66666666666666,"startXVariance":"0","startY":183.33333333333334,"startYVariance":"0","shapeIdList":["circle"],"startColor":{"hue":"0","hueVariance":"0","saturation":"100","saturationVariance":0,"luminance":"50","luminanceVariance":0},"blendMode":false,"alphaCurveType":"0"}
   };
   private setting1 = {
     name: "Abstract",
     description : "シンプルに動く丸や星や四角形",
     imageId : "graphic",
     property:
-      {"bgColor":"#724383","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"60","lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"3.5","initialSpeedVariance":"7.7","friction":"0.023","accelerationSpeed":"0","accelerationDirection":"116.5","startScale":"0.3","startScaleVariance":"1","finishScale":"0.26","finishScaleVariance":"0.54","startAlpha":"0.69","startAlphaVariance":"0.53","finishAlpha":"0","finishAlphaVariance":"0","startX":240.66666666666666,"startXVariance":"0","startY":183.33333333333334,"startYVariance":"0","shapeIdList":["circle","reverse_blur_circle","star"],"startColor":{"hue":"0","hueVariance":"0","satuation":"92","satuationVariance":0,"luminance":"100","luminanceVariance":0},"blendMode":true,"alphaCurveType":"0"}
+      {"bgColor":"#724383","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"60","lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"3.5","initialSpeedVariance":"7.7","friction":"0.023","accelerationSpeed":"0","accelerationDirection":"116.5","startScale":"0.3","startScaleVariance":"1","finishScale":"0.26","finishScaleVariance":"0.54","startAlpha":"0.69","startAlphaVariance":"0.53","finishAlpha":"0","finishAlphaVariance":"0","startX":240.66666666666666,"startXVariance":"0","startY":183.33333333333334,"startYVariance":"0","shapeIdList":["circle","reverse_blur_circle","star"],"startColor":{"hue":"0","hueVariance":"0","saturation":"92","saturationVariance":0,"luminance":"100","luminanceVariance":0},"blendMode":true,"alphaCurveType":"0"}
   };
 
   private setting2 = {
@@ -39,7 +39,7 @@ export class ParticleParamater {
     description: "紫の丸がキラキラしながら上方向に移動",
     imageId : "parple",
     property:
-     {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"100","lifeSpan":"50","lifeSpanVariance":"196","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"2","initialSpeedVariance":"3.7","friction":"0.038","accelerationSpeed":"0.13","accelerationDirection":"273.3","startScale":"0.76","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.35","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"594","startY":183.33333333333334,"startYVariance":"222","shapeIdList":["blur_circle","circle"],"startColor":{"hue":"312","hueVariance":"55","satuation":"71","satuationVariance":"78","luminance":"83","luminanceVariance":"16"},"blendMode":true,"alphaCurveType":"1"}
+     {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"100","lifeSpan":"50","lifeSpanVariance":"196","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"2","initialSpeedVariance":"3.7","friction":"0.038","accelerationSpeed":"0.13","accelerationDirection":"273.3","startScale":"0.76","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.35","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"594","startY":183.33333333333334,"startYVariance":"222","shapeIdList":["blur_circle","circle"],"startColor":{"hue":"312","hueVariance":"55","saturation":"71","saturationVariance":"78","luminance":"83","luminanceVariance":"16"},"blendMode":true,"alphaCurveType":"1"}
   };
 
   setting3 = {
@@ -47,14 +47,14 @@ export class ParticleParamater {
     description: "赤いぼけ玉を上方向に移動",
     imageId : "fire",
     property:
-    {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"200","lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"49","initialDirectionVariance":"360","initialSpeed":"3.8","initialSpeedVariance":"7.4","friction":"0.063","accelerationSpeed":"0.142","accelerationDirection":"275.8","startScale":"0.93","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"128","startY":183.33333333333334,"startYVariance":"148","shapeIdList":["blur_circle"],"startColor":{"hue":"17","hueVariance":"32","satuation":"91","satuationVariance":0,"luminance":"56","luminanceVariance":"16"},"blendMode":true,"alphaCurveType":"1"}
+    {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"200","lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"49","initialDirectionVariance":"360","initialSpeed":"3.8","initialSpeedVariance":"7.4","friction":"0.063","accelerationSpeed":"0.142","accelerationDirection":"275.8","startScale":"0.93","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"128","startY":183.33333333333334,"startYVariance":"148","shapeIdList":["blur_circle"],"startColor":{"hue":"17","hueVariance":"32","saturation":"91","saturationVariance":0,"luminance":"56","luminanceVariance":"16"},"blendMode":true,"alphaCurveType":"1"}
   };
   private setting4 = {
     name: "Spirit",
     description: "白に近いぼけ玉をキラキラさせた",
     imageId : "spirit",
     property:
-      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"30","lifeSpan":"343","lifeSpanVariance":"188","initialDirection":"102","initialDirectionVariance":"360","initialSpeed":"1.4","initialSpeedVariance":"0.7","friction":"0.022","accelerationSpeed":"0.042","accelerationDirection":"280.6","startScale":"1","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0.17","startAlpha":"1","startAlphaVariance":"0.58","finishAlpha":"0","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"178","startY":183.33333333333334,"startYVariance":"111","shapeIdList":["blur_circle"],"startColor":{"hue":"224","hueVariance":"41","satuation":"59","satuationVariance":"0","luminance":"85","luminanceVariance":"18"},"blendMode":true,"alphaCurveType":"1"}
+      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"30","lifeSpan":"343","lifeSpanVariance":"188","initialDirection":"102","initialDirectionVariance":"360","initialSpeed":"1.4","initialSpeedVariance":"0.7","friction":"0.022","accelerationSpeed":"0.042","accelerationDirection":"280.6","startScale":"1","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0.17","startAlpha":"1","startAlphaVariance":"0.58","finishAlpha":"0","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"178","startY":183.33333333333334,"startYVariance":"111","shapeIdList":["blur_circle"],"startColor":{"hue":"224","hueVariance":"41","saturation":"59","saturationVariance":"0","luminance":"85","luminanceVariance":"18"},"blendMode":true,"alphaCurveType":"1"}
   };
 
   private setting5 = {
@@ -62,7 +62,7 @@ export class ParticleParamater {
     description: "紫色の星をランダムに配置",
     imageId : "star",
     property:
-      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"100","lifeSpan":"27","lifeSpanVariance":"93","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"0","initialSpeedVariance":"10","friction":"0.008","accelerationSpeed":"0.2915","accelerationDirection":"129.5","startScale":"0.98","startScaleVariance":"1","finishScale":"0.07","finishScaleVariance":"0.23","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.34","finishAlphaVariance":0.5,"startX":333,"startXVariance":"117","startY":98,"startYVariance":"107","shapeIdList":["blur_circle","star","circle","kirakira"],"startColor":{"hue":"297","hueVariance":"99","satuation":"76","satuationVariance":"0","luminance":"77","luminanceVariance":"46"},"blendMode":true,"alphaCurveType":"1"}
+      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"100","lifeSpan":"27","lifeSpanVariance":"93","initialDirection":"0","initialDirectionVariance":"360","initialSpeed":"0","initialSpeedVariance":"10","friction":"0.008","accelerationSpeed":"0.2915","accelerationDirection":"129.5","startScale":"0.98","startScaleVariance":"1","finishScale":"0.07","finishScaleVariance":"0.23","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.34","finishAlphaVariance":0.5,"startX":333,"startXVariance":"117","startY":98,"startYVariance":"107","shapeIdList":["blur_circle","star","circle","kirakira"],"startColor":{"hue":"297","hueVariance":"99","saturation":"76","saturationVariance":"0","luminance":"77","luminanceVariance":"46"},"blendMode":true,"alphaCurveType":"1"}
   };
 
   private setting6 = {
@@ -99,16 +99,16 @@ export class ParticleParamater {
       "startColor": {
         "hue": "0",
         "hueVariance": "0",
-        "satuation": "0",
-        "satuationVariance": 0,
+        "saturation": "0",
+        "saturationVariance": 0,
         "luminance": "100",
         "luminanceVariance": "47"
       },
       "finishColor": {
         "hue": 50,
         "hueVariance": 0,
-        "satuation": 75,
-        "satuationVariance": 0,
+        "saturation": 75,
+        "saturationVariance": 0,
         "luminance": 75,
         "luminanceVariance": 0
       },
@@ -122,14 +122,14 @@ export class ParticleParamater {
     description : "赤い火球の落下を想定",
     imageId : "commet",
     property :
-      {"bgColor":"#00000","width":611.3333333333334,"height":624.6666666666666,"emitFrequency":300,"lifeSpan":"40","lifeSpanVariance":"0","initialDirection":"209.5","initialDirectionVariance":"155","initialSpeed":"2.6","initialSpeedVariance":"3.8","friction":"0.0085","accelerationSpeed":"0.835","accelerationDirection":"233.2","startScale":"1","startScaleVariance":"0.52","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"1","finishAlphaVariance":"0","startX":320,"startXVariance":"0","startY":349,"startYVariance":"0","shapeIdList":["blur_circle"],"startColor":{"hue":"17","hueVariance":"32","satuation":"100","satuationVariance":"45","luminance":"56","luminanceVariance":"19"},"blendMode":true,"alphaCurveType":"0"}
+      {"bgColor":"#00000","width":611.3333333333334,"height":624.6666666666666,"emitFrequency":300,"lifeSpan":"40","lifeSpanVariance":"0","initialDirection":"209.5","initialDirectionVariance":"155","initialSpeed":"2.6","initialSpeedVariance":"3.8","friction":"0.0085","accelerationSpeed":"0.835","accelerationDirection":"233.2","startScale":"1","startScaleVariance":"0.52","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"1","finishAlphaVariance":"0","startX":320,"startXVariance":"0","startY":349,"startYVariance":"0","shapeIdList":["blur_circle"],"startColor":{"hue":"17","hueVariance":"32","saturation":"100","saturationVariance":"45","luminance":"56","luminanceVariance":"19"},"blendMode":true,"alphaCurveType":"0"}
   };
   private setting8 = {
     name : "Heart",
     description : "ハートマークを上方向に移動",
     imageId : "heart_world",
     property :
-      {"bgColor":"#00000","width":611.3333333333334,"height":624.6666666666666,"emitFrequency":60,"lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"270","initialDirectionVariance":"360","initialSpeed":"1","initialSpeedVariance":"0","friction":"0.071","accelerationSpeed":"0.1045","accelerationDirection":"270","startScale":"0.5","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.8","finishAlphaVariance":"0","startX":309,"startXVariance":"597","startY":516,"startYVariance":"165","shapeIdList":["heart"],"startColor":{"hue":"335","hueVariance":"0","satuation":"78","satuationVariance":"0","luminance":"59","luminanceVariance":"50"},"blendMode":true,"alphaCurveType":"1"}
+      {"bgColor":"#00000","width":611.3333333333334,"height":624.6666666666666,"emitFrequency":60,"lifeSpan":"136","lifeSpanVariance":"27","initialDirection":"270","initialDirectionVariance":"360","initialSpeed":"1","initialSpeedVariance":"0","friction":"0.071","accelerationSpeed":"0.1045","accelerationDirection":"270","startScale":"0.5","startScaleVariance":"1","finishScale":"0","finishScaleVariance":"0","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.8","finishAlphaVariance":"0","startX":309,"startXVariance":"597","startY":516,"startYVariance":"165","shapeIdList":["heart"],"startColor":{"hue":"335","hueVariance":"0","saturation":"78","saturationVariance":"0","luminance":"59","luminanceVariance":"50"},"blendMode":true,"alphaCurveType":"1"}
   };
 
   private setting10 = {
@@ -137,7 +137,7 @@ export class ParticleParamater {
     description : "ハートマークを上方向に移動",
     imageId : "blue_light",
     property :
-      {"width":512,"height":512,"startX":146,"startY":251,"bgColor":"#00000","initialDirection":0,"initialDirectionVariance":"360","finishScale":0.18,"finishScaleVariance":0.68,"startAlpha":"1","startAlphaVariance":"0","emitFrequency":129,"lifeSpan":60,"lifeSpanVariance":80,"initialSpeed":18.8,"initialSpeedVariance":"3.7","friction":0.1165,"accelerationSpeed":0.4705,"accelerationDirection":"360","startScale":0.88,"startScaleVariance":"1","finishAlpha":0.51,"finishAlphaVariance":0.5,"startXVariance":73,"startYVariance":43,"shapeIdList":["circle","blur_circle"],"startColor":{"hue":195,"hueVariance":21,"satuation":70,"satuationVariance":"0","luminance":77,"luminanceVariance":37},"blendMode":true,"alphaCurveType":"1"}
+      {"width":512,"height":512,"startX":146,"startY":251,"bgColor":"#00000","initialDirection":0,"initialDirectionVariance":"360","finishScale":0.18,"finishScaleVariance":0.68,"startAlpha":"1","startAlphaVariance":"0","emitFrequency":129,"lifeSpan":60,"lifeSpanVariance":80,"initialSpeed":18.8,"initialSpeedVariance":"3.7","friction":0.1165,"accelerationSpeed":0.4705,"accelerationDirection":"360","startScale":0.88,"startScaleVariance":"1","finishAlpha":0.51,"finishAlphaVariance":0.5,"startXVariance":73,"startYVariance":43,"shapeIdList":["circle","blur_circle"],"startColor":{"hue":195,"hueVariance":21,"saturation":70,"saturationVariance":"0","luminance":77,"luminanceVariance":37},"blendMode":true,"alphaCurveType":"1"}
   };
 
   private setting9 = {
@@ -145,6 +145,6 @@ export class ParticleParamater {
     description : "星やぼけ玉を使用し、ちらつき",
     imageId : "green_kirakira",
     property :
-      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"38","lifeSpan":"27","lifeSpanVariance":"93","initialDirection":"0","initialDirectionVariance":"0","initialSpeed":"0","initialSpeedVariance":"0","friction":"0","accelerationSpeed":"0","accelerationDirection":"0","startScale":"0.98","startScaleVariance":"1","finishScale":"0.07","finishScaleVariance":"0.23","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.34","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"570","startY":183.33333333333334,"startYVariance":"98","shapeIdList":["blur_circle","kirakira2","kirakira","star"],"startColor":{"hue":"88","hueVariance":"99","satuation":"76","satuationVariance":"0","luminance":"77","luminanceVariance":"46"},"blendMode":true,"alphaCurveType":"1"}
+      {"bgColor":"#00000","width":481.3333333333333,"height":366.6666666666667,"emitFrequency":"38","lifeSpan":"27","lifeSpanVariance":"93","initialDirection":"0","initialDirectionVariance":"0","initialSpeed":"0","initialSpeedVariance":"0","friction":"0","accelerationSpeed":"0","accelerationDirection":"0","startScale":"0.98","startScaleVariance":"1","finishScale":"0.07","finishScaleVariance":"0.23","startAlpha":"1","startAlphaVariance":"0","finishAlpha":"0.34","finishAlphaVariance":0.5,"startX":240.66666666666666,"startXVariance":"570","startY":183.33333333333334,"startYVariance":"98","shapeIdList":["blur_circle","kirakira2","kirakira","star"],"startColor":{"hue":"88","hueVariance":"99","saturation":"76","saturationVariance":"0","luminance":"77","luminanceVariance":"46"},"blendMode":true,"alphaCurveType":"1"}
     };
 }
