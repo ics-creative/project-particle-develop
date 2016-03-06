e30734096d4db034148dc8ef2adbe9c1713b64bc
ics-kano <takeshi.kano@ics-web.jp>
2016-03-06 22:13:50 +0900

    Gulp：Autoprefixerの導入

--- core/gulpfile.js
+++ core/gulpfile.js
@@ -18,6 +18,10 @@ gulp.task("scssCompile", function () {
 	return gulp.src('scss/bootstrap.scss')
 		.pipe(sourcemaps.init())
 		.pipe(plugins.sass({outputStyle: 'compressed'}).on("error", plugins.sass.logError))
+		.pipe(plugins.autoprefixer({
+			browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"],
+			cascade: false
+		}))
 		.pipe(plugins.rename("index.css"))
 		.pipe(sourcemaps.write("../maps"))
 		.pipe(gulp.dest('css/'));
