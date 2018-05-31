bd69eda9012b2c95af49a26aecc61eb6226ff426
ics-ikeda <ikeda@ics-web.jp>
2018-05-31 15:56:54 +0900

    PWA対応

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+// BootstrapのJavaScript側の機能を読み込む
+import 'bootstrap';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
@@ -10,5 +12,19 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
-// BootstrapのJavaScript側の機能を読み込む
-import 'bootstrap';
+{
+  // Webフォントをあとから読み込む
+  const d = document;
+  const element = d.createElement('link');
+  element.setAttribute('rel', 'stylesheet');
+  element.setAttribute('src', 'https://fonts.googleapis.com/earlyaccess/notosansjapanese.css');
+  document.head.appendChild(element);
+}
+
+
+// サービスワーカーを起動する
+{
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js');
+  }
+}
